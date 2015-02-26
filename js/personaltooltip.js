8d34a54d43de8d5ead75eb0da0de772097d03034
karina-soni <karinasoni1@gmail.com>
2015-02-25 20:59:01 -0500

    getting closegit statusgit status

---
+++ js/personaltooltip.js
@@ -0,0 +1,133 @@
+        $(document).ready(function() {
+            $('#tooltip1').tooltipster({
+                maxWidth:400, 
+                content: $('<b>Disco was a lifestyle that paved the way for the modern gay and feminist movements we take for granted today.</b>')
+            });
+            
+            $('#tooltip2').tooltipster({
+                positionTracker: true,
+                maxWidth: 400, 
+                content: $('<img src="img/hippies.jpg">')
+            });
+            
+            $('#tooltip3').tooltipster({
+                content: $('<b>The only place disco fans COULD hear this music was at parties and clubs.</b>')
+            });
+            
+            $('#tooltip4').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                maxWidth:400,
+                content: $('<a href="http://www.theatlantic.com/politics/archive/2013/01/an-amazing-1969-account-of-the-stonewall-uprising/272467/" target="_blank">The Stonewall Riots in NYC are now considered the single most important event leading to gay liberation in the Unied States.</a>')
+            });
+            
+            $('#tooltip5').tooltipster({
+                maxWidth:400,
+                content: $('<b>Because the Loft was a private party destination and not a public dance club, police could not habitually raid and arrest gay partygoers.</b>')
+            });
+            
+            $('#tooltip6').tooltipster({
+                maxWidth:400,
+                position: "right",
+                content: $('<b>Beatmatching: the act of synchronizing the tempos of two recordings to enable a smooth transition between them in a set of uninterrupted music</b>')
+            });
+            
+            $('#tooltip7').tooltipster({
+                maxWidth:400,
+                content: $('<img src="img/davidm.jpg">')
+            });
+            $('#tooltip8').tooltipster({
+                maxWidth:400,
+                content: $('<b>The Garage was different from other clubs because it could hold over 3,000 people and was the second largest club in New York City in the late seventies.</b>')
+            });
+            
+            $('#tooltip9').tooltipster({
+                maxWidth:400,
+                maxHeight:400,
+                content: $('<img src="img/levan.jpg">')
+            });
+            
+            $('#tooltip10').tooltipster({
+                maxWidth:400,
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<a href="http://www.redbullmusicacademy.com/magazine/larry-levan-feature" target="_blank">Peter Shapiro, author of <i>Turn the Beat Around: The Secret History of Disco</i>, has written a great article on Levan. Check it out!</a>')
+            });
+            
+            $('#tooltip11').tooltipster({
+                maxWidth:400,
+                content: $('<b>The 12-inch single was more desirable to DJs over other records because they were easier to mix.</b>')
+            });
+            
+            $('#tooltip13').tooltipster({
+                maxWidth:400,
+                content: $('<b>Gibbons remixed version of the song later became the first commercially available 12-inch disco single in the world.</b>')
+            });
+            
+            $('#tooltip12').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/tGrKcqcHHcE" frameborder="0" allowfullscreen></iframe>')
+            });
+            
+            $('#tooltip14').tooltipster({
+                maxWidth:400,
+                content: $('<b> White disco groups such as ABBA, The BeeGees and KC and the Sunshine Band became successful with the help of radio airplay.</b>')
+            });
+            
+            $('#tooltip15').tooltipster({
+                maxWidth:400,
+                content: $('<img src="img/eltondiana.jpg"><p color="white">Elton John and Diana Ross</p>')
+            });
+            
+             $('#tooltip16').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/4zQpMnU6N4o?start=2773" frameborder="0" allowfullscreen></iframe>')
+            });
+             
+             $('#tooltip17').tooltipster({
+                maxWidth:400,
+                content: $('<img src="img/sesame.jpg"><p color="white">Yes, this happened.</p>')
+            });
+             
+             $('#tooltip18').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/I1CP1751wJA" frameborder="0" allowfullscreen></iframe>')
+            });
+            
+            $('#tooltip19').tooltipster({
+                maxWidth:400,
+                content: $('<b>Dahl exploded over 10,000 records in the middle of the baseball field, causing the second game to never happen. Over 50,000 people attended the event.</b>')
+            });
+            
+             $('#tooltip20').tooltipster({
+                maxWidth:400,
+                content: $('<img src="img/lol.jpg"><p color="white">I can see why.</p>')
+            });
+             
+              $('#tooltip21').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/SjaiKxSQy1w" frameborder="0" allowfullscreen></iframe>')
+            });
+              
+              $('#tooltip22').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/Jfc-knHB41U" frameborder="0" allowfullscreen></iframe>')
+            });
+              
+              $('#tooltip23').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/LOLE1YE_oFQ" frameborder="0" allowfullscreen></iframe>')
+            });
+              
+               $('#tooltip24').tooltipster({
+                interactive: true,
+                interactiveTolerance: 1000,
+                content: $('<iframe width="420" height="315" src="https://www.youtube.com/embed/kBKRYBwguqk" frameborder="0" allowfullscreen></iframe>')
+            });
+        });
